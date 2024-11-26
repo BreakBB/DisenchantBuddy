b06afc01db4015b0e600131318ccf016b2d34efc
BreakBB <b.boeing@online.de>
2024-11-26 19:59:13 +0100

    Fix usages of luassert and busted globals

--- DisenchantBuddy.test.lua
+++ DisenchantBuddy.test.lua
@@ -10,20 +10,22 @@ describe("DisenchantBuddy", function()
     end)
 
     it("should hook GameTooltip with OnTooltipSetItem", function()
-        assert.spy(_G.GameTooltip.HookScript).was_called_with(_G.GameTooltip, "OnTooltipSetItem", DisenchantBuddy.OnTooltipSetItem)
+        assert.spy(_G.GameTooltip.HookScript).was.called_with(_G.GameTooltip, "OnTooltipSetItem", DisenchantBuddy.OnTooltipSetItem)
     end)
 
     describe("OnTooltipSetItem", function()
         it("should show tooltip", function()
             local tooltip = {
-                GetItem = spy.new(function() return "Cookie's Tenderizer", "|cff1eff00|Hitem:5197:0:0:0:0:0:0:0:0:0:0|h[Cookie's Tenderizer]|h|r" end),
+                GetItem = spy.new(function()
+                    return "Cookie's Tenderizer", "|cff1eff00|Hitem:5197:0:0:0:0:0:0:0:0:0:0|h[Cookie's Tenderizer]|h|r"
+                end),
                 Show = spy.new()
             }
 
             DisenchantBuddy.OnTooltipSetItem(tooltip)
 
-            assert.spy(tooltip.GetItem).was_called()
-            assert.spy(tooltip.Show).was_called()
+            assert.spy(tooltip.GetItem).was.called()
+            assert.spy(tooltip.Show).was.called()
         end)
     end)
 end)
