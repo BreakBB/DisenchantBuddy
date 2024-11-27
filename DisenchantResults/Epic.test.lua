07e32ffc6a9980ce62bbe3d69354f255adda81c3
BreakBB <b.boeing@online.de>
2024-11-27 09:27:27 +0100

    Improve test names

--- DisenchantResults/Epic.test.lua
+++ DisenchantResults/Epic.test.lua
@@ -20,133 +20,133 @@ describe("GetEpicDisenchantResults", function()
         assert.is_nil(results)
     end)
 
-    it("should correct results level 5 items", function()
+    it("should return correct results for level 5 items", function()
         local results = GetEpicDisenchantResults(5)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 15 items", function()
+    it("should return correct results for level 15 items", function()
         local results = GetEpicDisenchantResults(15)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 16 items", function()
+    it("should return correct results for level 16 items", function()
         local results = GetEpicDisenchantResults(16)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 20 items", function()
+    it("should return correct results for level 20 items", function()
         local results = GetEpicDisenchantResults(20)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 21 items", function()
+    it("should return correct results for level 21 items", function()
         local results = GetEpicDisenchantResults(21)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 25 items", function()
+    it("should return correct results for level 25 items", function()
         local results = GetEpicDisenchantResults(25)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 26 items", function()
+    it("should return correct results for level 26 items", function()
         local results = GetEpicDisenchantResults(26)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 30 items", function()
+    it("should return correct results for level 30 items", function()
         local results = GetEpicDisenchantResults(30)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 31 items", function()
+    it("should return correct results for level 31 items", function()
         local results = GetEpicDisenchantResults(31)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 35 items", function()
+    it("should return correct results for level 35 items", function()
         local results = GetEpicDisenchantResults(35)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 36 items", function()
+    it("should return correct results for level 36 items", function()
         local results = GetEpicDisenchantResults(36)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 40 items", function()
+    it("should return correct results for level 40 items", function()
         local results = GetEpicDisenchantResults(40)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 41 items", function()
+    it("should return correct results for level 41 items", function()
         local results = GetEpicDisenchantResults(41)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 45 items", function()
+    it("should return correct results for level 45 items", function()
         local results = GetEpicDisenchantResults(45)
 
         assert.are_same({Materials.SMALL_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 46 items", function()
+    it("should return correct results for level 46 items", function()
         local results = GetEpicDisenchantResults(46)
 
         assert.are_same({Materials.LARGE_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 50 items", function()
+    it("should return correct results for level 50 items", function()
         local results = GetEpicDisenchantResults(50)
 
         assert.are_same({Materials.LARGE_RADIANT_SHARD}, results)
     end)
 
-    it("should correct results level 51 items", function()
+    it("should return correct results for level 51 items", function()
         local results = GetEpicDisenchantResults(51)
 
         assert.are_same({Materials.SMALL_BRILLIANT_SHARD}, results)
     end)
 
-    it("should correct results level 55 items", function()
+    it("should return correct results for level 55 items", function()
         local results = GetEpicDisenchantResults(55)
 
         assert.are_same({Materials.SMALL_BRILLIANT_SHARD}, results)
     end)
 
-    it("should correct results level 56 items", function()
+    it("should return correct results for level 56 items", function()
         local results = GetEpicDisenchantResults(56)
 
         assert.are_same({Materials.NEXUS_CRYSTAL}, results)
     end)
 
-    it("should correct results level 60 items", function()
+    it("should return correct results for level 60 items", function()
         local results = GetEpicDisenchantResults(60)
 
         assert.are_same({Materials.NEXUS_CRYSTAL}, results)
     end)
 
-    it("should correct results level 61 items", function()
+    it("should return correct results for level 61 items", function()
         local results = GetEpicDisenchantResults(61)
 
         assert.are_same({Materials.NEXUS_CRYSTAL}, results)
     end)
 
-    it("should correct results level 65 items", function()
+    it("should return correct results for level 65 items", function()
         local results = GetEpicDisenchantResults(65)
 
         assert.are_same({Materials.NEXUS_CRYSTAL}, results)
