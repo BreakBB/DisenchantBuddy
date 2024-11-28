07bc73c4c9fa1eb4a26e6fe490c0e3e092c2f79e
BreakBB <b.boeing@online.de>
2024-11-28 20:31:51 +0100

    Add GetMaterialsForUncommonWeapons

--- DisenchantResults/Uncommon.test.lua
+++ DisenchantResults/Uncommon.test.lua
@@ -3,6 +3,7 @@ describe("GetMaterialsForUncommonItem", function()
     ---@type Materials
     local Materials
     local GetMaterialsForUncommonArmor
+    local GetMaterialsForUncommonWeapons
 
     before_each(function()
         -- We use `loadfile` over `require` to be able to hand in our own environment
@@ -12,6 +13,7 @@ describe("GetMaterialsForUncommonItem", function()
         Materials = DisenchantBuddy.Materials
         loadfile("DisenchantResults/Uncommon.lua")("DisenchantBuddy", DisenchantBuddy)
         GetMaterialsForUncommonArmor = DisenchantBuddy.GetMaterialsForUncommonArmor
+        GetMaterialsForUncommonWeapons = DisenchantBuddy.GetMaterialsForUncommonWeapons
     end)
 
     describe("GetMaterialsForUncommonArmor", function()
@@ -235,6 +237,232 @@ describe("GetMaterialsForUncommonItem", function()
             assert.are_same({
                 {itemId = Materials.ILLUSION_DUST, probability = 75},
                 {itemId = Materials.GREATER_ETERNAL_ESSENCE, probability = 20},
+                {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 5},
+            }, results)
+        end)
+    end)
+
+    describe("GetMaterialsForUncommonWeapons", function()
+        it("should return nil for unhandled item level", function()
+            local results = GetMaterialsForUncommonWeapons(66)
+
+            assert.is_nil(results)
+        end)
+
+        it("should return correct results for level 5 items", function()
+            local results = GetMaterialsForUncommonWeapons(5)
+
+            assert.are_same({
+                {itemId = Materials.LESSER_MAGIC_ESSENCE, probability = 80},
+                {itemId = Materials.STRANGE_DUST, probability = 20},
+            }, results)
+        end)
+
+        it("should return correct results for level 15 items", function()
+            local results = GetMaterialsForUncommonWeapons(15)
+
+            assert.are_same({
+                {itemId = Materials.LESSER_MAGIC_ESSENCE, probability = 80},
+                {itemId = Materials.STRANGE_DUST, probability = 20},
+            }, results)
+        end)
+
+        it("should return correct results for level 16 items", function()
+            local results = GetMaterialsForUncommonWeapons(16)
+
+            assert.are_same({
+                {itemId = Materials.GREATER_MAGIC_ESSENCE, probability = 75},
+                {itemId = Materials.STRANGE_DUST, probability = 20},
+                {itemId = Materials.SMALL_GLIMMERING_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 20 items", function()
+            local results = GetMaterialsForUncommonWeapons(20)
+
+            assert.are_same({
+                {itemId = Materials.GREATER_MAGIC_ESSENCE, probability = 75},
+                {itemId = Materials.STRANGE_DUST, probability = 20},
+                {itemId = Materials.SMALL_GLIMMERING_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 21 items", function()
+            local results = GetMaterialsForUncommonWeapons(21)
+
+            assert.are_same({
+                {itemId = Materials.LESSER_ASTRAL_ESSENCE, probability = 75},
+                {itemId = Materials.STRANGE_DUST, probability = 15},
+                {itemId = Materials.SMALL_GLIMMERING_SHARD, probability = 10},
+            }, results)
+        end)
+
+        it("should return correct results for level 25 items", function()
+            local results = GetMaterialsForUncommonWeapons(25)
+
+            assert.are_same({
+                {itemId = Materials.LESSER_ASTRAL_ESSENCE, probability = 75},
+                {itemId = Materials.STRANGE_DUST, probability = 15},
+                {itemId = Materials.SMALL_GLIMMERING_SHARD, probability = 10},
+            }, results)
+        end)
+
+        it("should return correct results for level 26 items", function()
+            local results = GetMaterialsForUncommonWeapons(26)
+
+            assert.are_same({
+                {itemId = Materials.GREATER_ASTRAL_ESSENCE, probability = 75},
+                {itemId = Materials.SOUL_DUST, probability = 20},
+                {itemId = Materials.LARGE_GLIMMERING_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 30 items", function()
+            local results = GetMaterialsForUncommonWeapons(30)
+
+            assert.are_same({
+                {itemId = Materials.GREATER_ASTRAL_ESSENCE, probability = 75},
+                {itemId = Materials.SOUL_DUST, probability = 20},
+                {itemId = Materials.LARGE_GLIMMERING_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 31 items", function()
+            local results = GetMaterialsForUncommonWeapons(31)
+
+            assert.are_same({
+                {itemId = Materials.LESSER_MYSTIC_ESSENCE, probability = 75},
+                {itemId = Materials.SOUL_DUST, probability = 20},
+                {itemId = Materials.SMALL_GLOWING_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 35 items", function()
+            local results = GetMaterialsForUncommonWeapons(35)
+
+            assert.are_same({
+                {itemId = Materials.LESSER_MYSTIC_ESSENCE, probability = 75},
+                {itemId = Materials.SOUL_DUST, probability = 20},
+                {itemId = Materials.SMALL_GLOWING_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 36 items", function()
+            local results = GetMaterialsForUncommonWeapons(36)
+
+            assert.are_same({
+                {itemId = Materials.GREATER_MYSTIC_ESSENCE, probability = 75},
+                {itemId = Materials.VISION_DUST, probability = 20},
+                {itemId = Materials.LARGE_GLOWING_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 40 items", function()
+            local results = GetMaterialsForUncommonWeapons(40)
+
+            assert.are_same({
+                {itemId = Materials.GREATER_MYSTIC_ESSENCE, probability = 75},
+                {itemId = Materials.VISION_DUST, probability = 20},
+                {itemId = Materials.LARGE_GLOWING_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 41 items", function()
+            local results = GetMaterialsForUncommonWeapons(41)
+
+            assert.are_same({
+                {itemId = Materials.LESSER_NETHER_ESSENCE, probability = 75},
+                {itemId = Materials.VISION_DUST, probability = 20},
+                {itemId = Materials.SMALL_RADIANT_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 45 items", function()
+            local results = GetMaterialsForUncommonWeapons(45)
+
+            assert.are_same({
+                {itemId = Materials.LESSER_NETHER_ESSENCE, probability = 75},
+                {itemId = Materials.VISION_DUST, probability = 20},
+                {itemId = Materials.SMALL_RADIANT_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 46 items", function()
+            local results = GetMaterialsForUncommonWeapons(46)
+
+            assert.are_same({
+                {itemId = Materials.GREATER_NETHER_ESSENCE, probability = 75},
+                {itemId = Materials.DREAM_DUST, probability = 20},
+                {itemId = Materials.LARGE_RADIANT_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 50 items", function()
+            local results = GetMaterialsForUncommonWeapons(50)
+
+            assert.are_same({
+                {itemId = Materials.GREATER_NETHER_ESSENCE, probability = 75},
+                {itemId = Materials.DREAM_DUST, probability = 20},
+                {itemId = Materials.LARGE_RADIANT_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 51 items", function()
+            local results = GetMaterialsForUncommonWeapons(51)
+
+            assert.are_same({
+                {itemId = Materials.LESSER_ETERNAL_ESSENCE, probability = 75},
+                {itemId = Materials.DREAM_DUST, probability = 20},
+                {itemId = Materials.SMALL_BRILLIANT_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 55 items", function()
+            local results = GetMaterialsForUncommonWeapons(55)
+
+            assert.are_same({
+                {itemId = Materials.LESSER_ETERNAL_ESSENCE, probability = 75},
+                {itemId = Materials.DREAM_DUST, probability = 20},
+                {itemId = Materials.SMALL_BRILLIANT_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 56 items", function()
+            local results = GetMaterialsForUncommonWeapons(56)
+
+            assert.are_same({
+                {itemId = Materials.GREATER_ETERNAL_ESSENCE, probability = 75},
+                {itemId = Materials.ILLUSION_DUST, probability = 20},
+                {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 60 items", function()
+            local results = GetMaterialsForUncommonWeapons(60)
+
+            assert.are_same({
+                {itemId = Materials.GREATER_ETERNAL_ESSENCE, probability = 75},
+                {itemId = Materials.ILLUSION_DUST, probability = 20},
+                {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 61 items", function()
+            local results = GetMaterialsForUncommonWeapons(61)
+
+            assert.are_same({
+                {itemId = Materials.GREATER_ETERNAL_ESSENCE, probability = 75},
+                {itemId = Materials.ILLUSION_DUST, probability = 20},
+                {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 5},
+            }, results)
+        end)
+
+        it("should return correct results for level 65 items", function()
+            local results = GetMaterialsForUncommonWeapons(65)
+
+            assert.are_same({
+                {itemId = Materials.GREATER_ETERNAL_ESSENCE, probability = 75},
+                {itemId = Materials.ILLUSION_DUST, probability = 20},
                 {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 5},
             }, results)
         end)
