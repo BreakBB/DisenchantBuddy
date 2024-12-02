0934f0fa91fb60b3773223678d43a4035e381186
BreakBB <b.boeing@online.de>
2024-12-02 07:45:46 +0100

    Complete quantities for rare items

--- DisenchantResults/Rare.lua
+++ DisenchantResults/Rare.lua
@@ -9,21 +9,21 @@ function DisenchantBuddy.GetMaterialsForRareItem(itemLevel)
     if itemLevel <= 25 then
         return {{itemId = Materials.SMALL_GLIMMERING_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}
     elseif itemLevel <= 30 then
-        return {{itemId = Materials.LARGE_GLIMMERING_SHARD, probability = 100}}
+        return {{itemId = Materials.LARGE_GLIMMERING_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}
     elseif itemLevel <= 35 then
-        return {{itemId = Materials.SMALL_GLOWING_SHARD, probability = 100}}
+        return {{itemId = Materials.SMALL_GLOWING_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}
     elseif itemLevel <= 40 then
-        return {{itemId = Materials.LARGE_GLOWING_SHARD, probability = 100}}
+        return {{itemId = Materials.LARGE_GLOWING_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}
     elseif itemLevel <= 45 then
-        return {{itemId = Materials.SMALL_RADIANT_SHARD, probability = 100}}
+        return {{itemId = Materials.SMALL_RADIANT_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}
     elseif itemLevel <= 50 then
-        return {{itemId = Materials.LARGE_RADIANT_SHARD, probability = 100}}
+        return {{itemId = Materials.LARGE_RADIANT_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}
     elseif itemLevel <= 55 then
-        return {{itemId = Materials.SMALL_BRILLIANT_SHARD, probability = 100}}
+        return {{itemId = Materials.SMALL_BRILLIANT_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}
     elseif itemLevel <= 65 then
         return {
-            {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 99.5},
-            {itemId = Materials.NEXUS_CRYSTAL, probability = 0.5},
+            {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 99.5, minQuantity = 1, maxQuantity = 1},
+            {itemId = Materials.NEXUS_CRYSTAL, probability = 0.5, minQuantity = 1, maxQuantity = 1},
         }
     else
         return nil
--- DisenchantResults/Rare.test.lua
+++ DisenchantResults/Rare.test.lua
@@ -59,81 +59,81 @@ describe("GetMaterialsForRareItem", function()
     it("should return correct results for level 26 items", function()
         local results = GetMaterialsForRareItem(26)
 
-        assert.are_same({{itemId = Materials.LARGE_GLIMMERING_SHARD, probability = 100}}, results)
+        assert.are_same({{itemId = Materials.LARGE_GLIMMERING_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}, results)
     end)
 
     it("should return correct results for level 30 items", function()
         local results = GetMaterialsForRareItem(30)
 
-        assert.are_same({{itemId = Materials.LARGE_GLIMMERING_SHARD, probability = 100}}, results)
+        assert.are_same({{itemId = Materials.LARGE_GLIMMERING_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}, results)
     end)
 
     it("should return correct results for level 31 items", function()
         local results = GetMaterialsForRareItem(31)
 
-        assert.are_same({{itemId = Materials.SMALL_GLOWING_SHARD, probability = 100}}, results)
+        assert.are_same({{itemId = Materials.SMALL_GLOWING_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}, results)
     end)
 
     it("should return correct results for level 35 items", function()
         local results = GetMaterialsForRareItem(35)
 
-        assert.are_same({{itemId = Materials.SMALL_GLOWING_SHARD, probability = 100}}, results)
+        assert.are_same({{itemId = Materials.SMALL_GLOWING_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}, results)
     end)
 
     it("should return correct results for level 36 items", function()
         local results = GetMaterialsForRareItem(36)
 
-        assert.are_same({{itemId = Materials.LARGE_GLOWING_SHARD, probability = 100}}, results)
+        assert.are_same({{itemId = Materials.LARGE_GLOWING_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}, results)
     end)
 
     it("should return correct results for level 40 items", function()
         local results = GetMaterialsForRareItem(40)
 
-        assert.are_same({{itemId = Materials.LARGE_GLOWING_SHARD, probability = 100}}, results)
+        assert.are_same({{itemId = Materials.LARGE_GLOWING_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}, results)
     end)
 
     it("should return correct results for level 41 items", function()
         local results = GetMaterialsForRareItem(41)
 
-        assert.are_same({{itemId = Materials.SMALL_RADIANT_SHARD, probability = 100}}, results)
+        assert.are_same({{itemId = Materials.SMALL_RADIANT_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}, results)
     end)
 
     it("should return correct results for level 45 items", function()
         local results = GetMaterialsForRareItem(45)
 
-        assert.are_same({{itemId = Materials.SMALL_RADIANT_SHARD, probability = 100}}, results)
+        assert.are_same({{itemId = Materials.SMALL_RADIANT_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}, results)
     end)
 
     it("should return correct results for level 46 items", function()
         local results = GetMaterialsForRareItem(46)
 
-        assert.are_same({{itemId = Materials.LARGE_RADIANT_SHARD, probability = 100}}, results)
+        assert.are_same({{itemId = Materials.LARGE_RADIANT_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}, results)
     end)
 
     it("should return correct results for level 50 items", function()
         local results = GetMaterialsForRareItem(50)
 
-        assert.are_same({{itemId = Materials.LARGE_RADIANT_SHARD, probability = 100}}, results)
+        assert.are_same({{itemId = Materials.LARGE_RADIANT_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}, results)
     end)
 
     it("should return correct results for level 51 items", function()
         local results = GetMaterialsForRareItem(51)
 
-        assert.are_same({{itemId = Materials.SMALL_BRILLIANT_SHARD, probability = 100}}, results)
+        assert.are_same({{itemId = Materials.SMALL_BRILLIANT_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}, results)
     end)
 
     it("should return correct results for level 55 items", function()
         local results = GetMaterialsForRareItem(55)
 
-        assert.are_same({{itemId = Materials.SMALL_BRILLIANT_SHARD, probability = 100}}, results)
+        assert.are_same({{itemId = Materials.SMALL_BRILLIANT_SHARD, probability = 100, minQuantity = 1, maxQuantity = 1}}, results)
     end)
 
     it("should return correct results for level 56 items", function()
         local results = GetMaterialsForRareItem(56)
 
         assert.are_same({
-            {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 99.5},
-            {itemId = Materials.NEXUS_CRYSTAL, probability = 0.5},
+            {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 99.5, minQuantity = 1, maxQuantity = 1},
+            {itemId = Materials.NEXUS_CRYSTAL, probability = 0.5, minQuantity = 1, maxQuantity = 1},
         }, results)
     end)
 
@@ -141,8 +141,8 @@ describe("GetMaterialsForRareItem", function()
         local results = GetMaterialsForRareItem(60)
 
         assert.are_same({
-            {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 99.5},
-            {itemId = Materials.NEXUS_CRYSTAL, probability = 0.5},
+            {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 99.5, minQuantity = 1, maxQuantity = 1},
+            {itemId = Materials.NEXUS_CRYSTAL, probability = 0.5, minQuantity = 1, maxQuantity = 1},
         }, results)
     end)
 
@@ -150,8 +150,8 @@ describe("GetMaterialsForRareItem", function()
         local results = GetMaterialsForRareItem(61)
 
         assert.are_same({
-            {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 99.5},
-            {itemId = Materials.NEXUS_CRYSTAL, probability = 0.5},
+            {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 99.5, minQuantity = 1, maxQuantity = 1},
+            {itemId = Materials.NEXUS_CRYSTAL, probability = 0.5, minQuantity = 1, maxQuantity = 1},
         }, results)
     end)
 
@@ -159,8 +159,8 @@ describe("GetMaterialsForRareItem", function()
         local results = GetMaterialsForRareItem(65)
 
         assert.are_same({
-            {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 99.5},
-            {itemId = Materials.NEXUS_CRYSTAL, probability = 0.5},
+            {itemId = Materials.LARGE_BRILLIANT_SHARD, probability = 99.5, minQuantity = 1, maxQuantity = 1},
+            {itemId = Materials.NEXUS_CRYSTAL, probability = 0.5, minQuantity = 1, maxQuantity = 1},
         }, results)
     end)
 end)
